3d49f80ce45e099f2d92e120ff02de64dec38cb2
JonatasDuarte <42140987+JonatasDuarte@users.noreply.github.com>
2020-04-03 14:02:08 -0300

    Doing user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,37 +1,77 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from 'react';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-import api from "../../services/api";
+import api from '../../services/api';
 
-import { Container } from "./styles"
+import { Container } from './styles';
 
-import {MdStar, MdCallSplit} from "react-icons"
+import { MdStar, MdCallSplit } from 'react-icons/md';
 
 export default function User() {
   const { username } = useParams();
 
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    async function loadRepos() {
+      const { data } = await api.get(`users/${username}/repos`);
+
+      setRepos(
+        data.map(repo => ({
+          name: repo.name,
+          url: repo.hmtl_url,
+          des: repo.description,
+          language: repo.language,
+          star: repo.stargazers_count,
+          forks: repo.forks_count,
+        }))
+      );
+    }
+
+    loadRepos();
+  }, [username]);
+
   const handleShowRepo = useCallback(
+    link => {
+      window.open(link, '_blank');
+    },
+    [username]
+  );
+  
 
+  return(  
+    <Container> 
+      
+      <RepoList repos={repos}/>
 
-
-  )
-
-  return;
+    </Container>
+  );
 }
 
-// /users/:username/repos
 /*
-  [
-     {
-         "html_url": "https://github.com/jarthursantos/base-react-electron",
-         "name": "base-react-electron",
-         "description": "Base code to produce a Electron application using ReactJS to make the UI",
-         "language": "JavaScript",
-         "stargazers_count": 0,
-         "forks_count": 0,
-     },
-  ]
+
+// yarn eslint --init
+
+// .eslintrc.js
+
+// yarn add prettier eslint-config-prettier eslint-plugin-prettier -D     
+
+// /users/:username/repos
+  
+
+  
+"editor.codeActionsOnSave": {
+  "source.fixAll.eslint": true
+},
+
+"emmet.includeLanguages": {
+  "javascript": "javascriptreact"
+},
+"emmet.syntaxProfiles": {
+  "javascript": "jsx"
+},
+
 */
 
 // git add .
